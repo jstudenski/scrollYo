2c21486e3fd495115de9dbc3bc7b32e66ecb2ee8
jstudenski <jstudenski@gmail.com>
2018-10-07 21:21:17 -0500

    added SASS complier

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './normalize.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
 require('typeface-roboto');
 
 ReactDOM.render(<App />, document.getElementById('root'));
